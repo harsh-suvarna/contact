351fab81f2bc99707175c838a7562348e6748222
harsh-suvarna <harshendrapoojary29355@gmail.com>
2024-12-19 10:19:59 +0530

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navigation from './componenets/navigation/Navigation'
+import Header from './componenets/header/Header'
+import Hero from './componenets/Hero/Hero'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Navigation/>
+      <Header/>
+      <Hero/>
+
+    </>
+  )
+}
+
+export default App
